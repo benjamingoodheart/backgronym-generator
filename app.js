1bb60cc1b37ac5a1b3c4d86a3da8e1ca137e4259
bengoodheart <bgoodheart@fordham.edu>
2023-05-30 17:38:05 -0400

    random

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const fs = require('fs/promises');
 const words = require('./words_dictionary.json')
 const four_letters = []
 
+
 function filterWordList(){
     for (const word in words){
         if (word.length === 4) {
@@ -11,8 +12,11 @@ function filterWordList(){
     }
 }
 
-function getRandomInt(){
-    return Math.floor(Math.random()*1000)
+function getRandomInt(min, max){
+    min = Math.ceil(min)
+    max = Math.floor(max)
+
+    return Math.floor(Math.random()* (max- min) + min);
 }
 
 function matches(int, i){
@@ -20,16 +24,14 @@ function matches(int, i){
 }
 
 function getRandomWord(){
-    let int = getRandomInt()
+    let int = getRandomInt(1, four_letters.length)
     console.log(int)
-    /**
     for (let i = 0; i < four_letters.length; i++) {
-        console.log(four_letters[i])
-    } */
-
-
+        if(matches(int, i)){
+            console.log(four_letters[int])
+        }
+    }
     
-
 }
 
 filterWordList()
