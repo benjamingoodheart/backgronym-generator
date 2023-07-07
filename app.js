09faa96ddc6f4f9eda9c4f743a78c0017c046598
bengoodheart <bgoodheart@fordham.edu>
2023-07-07 08:28:03 -0400

    refined a little
    still need to refactor to a class

--- app.js
+++ app.js
@@ -4,11 +4,12 @@ const axios = require("axios");
 const BASE_URL = "https://api.dictionaryapi.dev/api/v2/entries/en/";
 const fetched = false
 
-function filterByFourLetters() {
+
+function filterByLength(x) {
   const four_letters = [];
 
   for (const word in words) {
-    if (word.length === 4) {
+    if (word.length === x) {
       four_letters.push(word);
     }
   }
@@ -30,14 +31,13 @@ function getRandomWord(wordArr) {
   let int = getRandomInt(1, wordArr.length);
   for (let i = 0; i < wordArr.length; i++) {
     if (matches(int, i)) {
-      console.log(wordArr[int]);
       return wordArr[int];
     }
   }
 }
 
 async function getDefinitions(word) {
-     
+  
   const endpoint = `${BASE_URL}${word}`;
   try {
     await axios.get(endpoint).then((response) => {
@@ -46,9 +46,18 @@ async function getDefinitions(word) {
       return definitions;
     });
   } catch (err) {
-    console.log(err);
+    console.log(`Looks like we cant find a def for: ${word}` )
     return err;
   }
+}
+function filterByPrefix(prefix){
+  const filteredList = []
+  const regex = new RegExp(`${prefix}*`)
+
+  console.log(regex.test("baser"))
+  //TODO: IMplemenet filter
+
+ return regex
 }
 
 function filterByFirstLetter(letter) {
@@ -62,16 +71,31 @@ function filterByFirstLetter(letter) {
   return filteredList;
 }
 
-function backronym(word) {
+
+function backgronym(word) {
   for (const w in word) {
     let letter = word[w];
     let temp = filterByFirstLetter(letter);
-    getRandomWord(temp);
+    console.log(getRandomWord(temp));
   }
 }
 
-const fl = filterByFourLetters();
+const fl = filterByLength(4);
 const term = getRandomWord(fl);
+const definitionsResults = ( async ()=>{
+  return getDefinitions(term)
+})
 
-const response = getDefinitions(term);
+function mainDriver(){
+  console.log("The term is...")
+  console.log(term)
+  console.log(`And a backgronym for ${term} is...`)
+  console.log(backgronym(term))
+  console.log(`-------- Wait --------`)
+  console.log(`What the hell does ${term} mean?`)
+  console.log(definitionsResults())
+}
 
+
+
+mainDriver();
