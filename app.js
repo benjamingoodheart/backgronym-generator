e3adfc6a886be65444f3ce65658453fb1756a8cb
benjamingoodheart <bgoodheart@fordham.edu>
2024-05-23 23:41:58 -0400

    added demos

--- app.js
+++ app.js
@@ -2,65 +2,23 @@ const fs = require("fs/promises");
 const words = require("./words_dictionary.json");
 const axios = require("axios");
 const BASE_URL = "https://api.dictionaryapi.dev/api/v2/entries/en/";
-const fetched = false
+const fetched = false;
+const fl = filterByLength(getRandomInt(4, 8));
+const term = getRandomWord(fl);
 
+function backgronym(word) {
+  for (const w in word) {
+    let letter = word[w];
+    let temp = filterByFirstLetter(letter);
 
-function filterByLength(x) {
-  const four_letters = [];
-
-  for (const word in words) {
-    if (word.length === x) {
-      four_letters.push(word);
-    }
+    console.log(getRandomWord(temp));
   }
-  return four_letters;
+  return `\n~~~~~~~~~~~ ⍞⍠ 🆒🆒 ⍠⍞ ~~~~~~~~~~`;
 }
 
-function getRandomInt(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-
-  return Math.floor(Math.random() * (max - min) + min);
-}
-
-function matches(int, i) {
-  return int === i ? true : false;
-}
-
-function getRandomWord(wordArr) {
-  let int = getRandomInt(1, wordArr.length);
-  for (let i = 0; i < wordArr.length; i++) {
-    if (matches(int, i)) {
-      return wordArr[int];
-    }
-  }
-}
-
-
-async function getDefinitions(word) {
-  const endpoint = `${BASE_URL}${word}`;
-  try {
-    await axios.get(endpoint).then((response) => {
-      const definitions = Object.freeze(response.data[0].meanings[0].definitions);
-      console.log(definitions);
-      return definitions;
-    });
-  } catch (err) {
-    console.log(`😵😵😵😵😵😵😵😵😵😵😵😵😵😵😵😵`)
-    console.log(`Looks like we cant find a def for: ${word.toUpperCase()}` )
-    console.log(`It's now up to you to guide the word ${word.toUpperCase()} toward meaning...Or just consult another dictionary.`)
-    return err;
-  }
-}
-function filterByPrefix(prefix){
-  const filteredList = []
-  const regex = new RegExp(`${prefix}*`)
-
-  console.log(regex.test("baser"))
-  //TODO: IMplemenet filter
-
- return regex
-}
+const definitionsResults = async () => {
+  return getDefinitions(term);
+};
 
 function filterByFirstLetter(letter) {
   const filteredList = [];
@@ -73,34 +31,78 @@ function filterByFirstLetter(letter) {
   return filteredList;
 }
 
+function filterByLength(x) {
+  const four_letters = [];
 
-function backgronym(word) {
-  for (const w in word) {
-    let letter = word[w];
-    let temp = filterByFirstLetter(letter);
-
-    console.log(getRandomWord(temp));
+  for (const word in words) {
+    if (word.length === x) {
+      four_letters.push(word);
+    }
   }
-  return `\n~~~~~~~~~~~ ⍞⍠ 🆒🆒 ⍠⍞ ~~~~~~~~~~`
+  return four_letters;
 }
 
-const fl = filterByLength(getRandomInt(4, 8));
-const term = getRandomWord(fl);
+function filterByPrefix(prefix) {
+  const filteredList = [];
+  const regex = new RegExp(`${prefix}*`);
 
+  //TODO: Implement filter
 
-const definitionsResults = ( async ()=>{
-  return getDefinitions(term)
-})
+  return regex;
+}
 
-function mainDriver(){
-  console.log("The term is...")
-  console.log(term)
-  console.log(`And a backgronym for ${term.toUpperCase()} is...`)
-  console.log(backgronym(term))
-  console.log(`-------__⁉🙉_W A I T_🙉⁉__-------`)
-  console.log(``)
-  console.log(`What the heck does ${term.toUpperCase()} mean?`)
-  console.log(definitionsResults())
+async function getDefinitions(word) {
+  const endpoint = `${BASE_URL}${word}`;
+
+  try{
+    await axios.get(endpoint).then((response) => {
+
+      const definitions = Object.freeze(
+        response.data[0].meanings[0].definitions
+      );
+      console.log(definitions);
+      return definitions;
+    });
+  } catch (err){
+
+    console.log(`😵😵😵😵😵😵😵😵😵😵😵😵😵😵😵😵`);
+    console.log(`Looks like we cant find a def for: ${word.toUpperCase()}`);
+    console.log(
+      `It's now up to you to guide the word ${word.toUpperCase()} toward meaning...Or just consult another dictionary.`
+    ); 
+  }
+      
+}
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+function getRandomWord(wordArr) {
+  let int = getRandomInt(1, wordArr.length);
+  for (let i = 0; i < wordArr.length; i++) {
+    if (matches(int, i)) {
+      return wordArr[int];
+    }
+  }
+}
+
+function mainDriver() {
+  console.log("The term is...");
+  console.log(term);
+  console.log(`And a backgronym for ${term.toUpperCase()} is...`);
+  console.log(backgronym(term));
+  console.log(`-------__⁉🙉_W A I T_🙉⁉__-------`);
+  console.log(``);
+  console.log(`What the heck does ${term.toUpperCase()} mean?`);
+  console.log(definitionsResults());
+}
+
+function matches(int, i) {
+  return int === i ? true : false;
 }
 
 
